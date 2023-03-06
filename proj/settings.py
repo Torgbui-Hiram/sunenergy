8f6c3a5b3e67f999c47f5abb363972e8bcfdbab4
Torgbui-Hiram <hiramkumado@gmail.com>
2023-03-06 09:51:05 +0000

    minor

--- proj/settings.py
+++ proj/settings.py
@@ -27,7 +27,7 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-
+CSRF_TRUSTED_ORIGINS = ['https://sunenergy-sol.up.railway.app']
 ALLOWED_HOSTS = ['*']
 
 
